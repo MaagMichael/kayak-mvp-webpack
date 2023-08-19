d475134569ab4209b878f522fe1f5b0b473a4a62
MaagMichael <maagm@t-online.de>
2023-08-19 13:38:29 +0200

    go top button

--- src/components/ScrollTop.jsx
+++ src/components/ScrollTop.jsx
@@ -7,28 +7,31 @@ import "./ScrollTop.css";
 import IconTop from "../images/CircleUpRounded.svg";
 
 export default function ScrollTop() {
+
   // The back-to-top button is hidden at the beginning
   const [showUpButton, setShowUpButton] = useState(false);
 
-//   const changenavcolor = () => {
+  //   check when to make button visible (method 1)
+//   const showtop = () => {
 //     // console.log(window.scrollY);
-//     if (window.scrollY > 50) {
-//       setNavcolor(true);
+//     if (window.scrollY > 300) {
+//       setShowUpButton(true);
 //     } else {
-//       setNavcolor(false);
+//       setShowUpButton(false);
 //     }
 //   };
-//   window.addEventListener("scroll", changenavcolor);
+//   window.addEventListener("scroll", showtop);
 
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 300) {
-        setShowUpButton(true);
-      } else {
-        setShowUpButton(false);
-      }
-    });
-  }, []);
+  //   check when to make button visible (method 2)
+    useEffect(() => {
+      window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+          setShowUpButton(true);
+        } else {
+          setShowUpButton(false);
+        }
+      });
+    }, []);
 
   // This function will scroll the window to the top
   const scrollToTop = () => {
